15bf8d99224149493bea50aa379e3b6a8e2095e9
Shivangi Sharma <62565739+ShivangiSharma77@users.noreply.github.com>
2021-05-26 22:54:58 +0530

    Quadilateral.java

---
+++ Assignment1/Quadrilateral/Quadilateral.java
@@ -0,0 +1,15 @@
+public class Quadilateral
+{
+    protected int x1,x2,x3,x4,y1,y2,y3,y4;
+    protected void setCoordinate(int z1,int z2,int z3,int z4,int w1,int w2,int w3,int w4)
+    {
+        x1=z1;
+        y1=z2;
+        x2=z3;
+        y2=z4;
+        x3=w1;
+        y3=w2;
+        x4=w3;
+        y4=w4;
+    }
+}
