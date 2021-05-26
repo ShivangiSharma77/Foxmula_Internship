6109467bb9533133e6b7a2ce46a86dd671ec3fdd
Shivangi Sharma <62565739+ShivangiSharma77@users.noreply.github.com>
2021-05-26 23:03:31 +0530

    Update Calling_Quadilateral.java

--- Assignment1/Quadrilateral/Calling_Quadilateral.java
+++ Assignment1/Quadrilateral/Calling_Quadilateral.java
@@ -3,12 +3,10 @@ class Calling_Quadilateral
 {
     public static void main(String args[])
     {
-  
-		Rectangle1 r = new Rectangle1(0, 0, 4, 0, 4, 2, 0, 2);
-		Square1 s = new Square1(0, 0, 2, 0, 2 ,2, 0, 2);
-		Parallelogram1 p = new Parallelogram1(0, 0, 2, 0, 3, 1, 1, 1, 7);
-		Trapezoid1 t = new Trapezoid1(0, 0, 5, 0, 4, 4, 1, 4, 8);
-		
+                Rectangle1 r = new Rectangle1(50,50,250,50,250,150,50,150);
+		Square1 s = new Square1(0, 0, 70, 0, 70 ,70, 0, 70);
+		Parallelogram1 p = new Parallelogram1(50,50,250,50,150,150,0,150,180);
+		Trapezoid1 t = new Trapezoid1(50,50,250,50,150,150,0,150,180);
 		System.out.println("The area of Square is " + s.area());
 		System.out.println("The area of Rectange is : " + r.area());
 		System.out.println("The area of Parallelogram is " + p.area());
