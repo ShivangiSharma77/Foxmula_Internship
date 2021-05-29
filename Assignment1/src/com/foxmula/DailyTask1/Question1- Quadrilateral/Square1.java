fab4afe8397b0d502cd60be610d6afa95d5026a4
Shivangi Sharma <62565739+ShivangiSharma77@users.noreply.github.com>
2021-05-29 16:43:22 +0530

    Assignment1

---
+++ Assignment1/src/com/foxmula/DailyTask1/Question1- Quadrilateral/Square1.java
@@ -0,0 +1,13 @@
+package Assignment1;
+class Square1 extends Quadilateral
+{
+    Square1(int z1,int z2,int z3,int z4,int w1,int w2,int w3,int w4)
+    {
+        setCoordinate(z1,z2,z3,z4,w1,w2,w3,w4);
+    }
+    int area()
+    {
+        int d=(int)Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+        return d*d;
+    }
+}
---
+++ Assignment1/src/com/foxmula/DailyTask1/Question1- Quadrilateral/Trapezoid1.java
@@ -0,0 +1,16 @@
+package Assignment1;
+class Trapezoid1 extends Quadilateral
+{
+    private int ht;
+    Trapezoid1(int z1,int z2,int z3,int z4,int w1,int w2,int w3,int w4, int ht)
+    {
+    	setCoordinate(z1,z2,z3,z4,w1,w2,w3,w4);
+        this.ht=ht;
+    }
+    int area()
+    {
+        int d1=(int)Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+        int d2=(int)Math.sqrt((x3-x4)*(x3-x4)+(y3-y4)*(y3-y4));
+        return (int)((d1+d2)*ht)/2;
+    }
+}
